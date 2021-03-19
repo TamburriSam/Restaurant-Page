e932b78e98ba238465c8c3df0bd21a6533763513
tamburrisam <tamburrisam@gmail.com>
2021-03-19 13:02:32 -0400

    added page content

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
-import {  makeBackground, makeTitle, makeSubtitle, makeNav } from './renderElement.js'
+import { makeAdvertisement, makeBackground, headingDiv, makeTitle, makeNav, homeContent, info } from './renderElement.js'
 //make factory
 
 
 const contentDiv = document.querySelector('#content');
 contentDiv.setAttribute("id", "mainpic");
 
+//tabbed event listeners
+const home = document.querySelector('.home')
+
+/* home.addEventListener('click', function(){
+    clear();
+}) */
+
+function homeClick(){
+    home.addEventListener('click', function(){
+        console.log('j')
+    })
+}
 
 
+makeAdvertisement();
+makeBackground(contentDiv); //background container
+headingDiv();
+makeTitle();
+makeNav();
+homeContent();
+info();
 
-makeBackground(contentDiv)
-makeTitle(contentDiv)
-makeSubtitle(contentDiv)
+//makeTitle(contentDiv)
+//makeSubtitle(contentDiv)
