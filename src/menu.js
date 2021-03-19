0bddb4b6295db25edafa3900627df1c44e771dc0
tamburrisam <tamburrisam@gmail.com>
2021-03-19 16:57:58 -0400

    added txt decor on menu item

--- src/menu.js
+++ src/menu.js
@@ -22,7 +22,11 @@ function testBackground(){
 
  bigDiv.appendChild(newDiv)
 
+const figDiv = document.createElement('div');
+figDiv.setAttribute('class', 'foodDivs');
+figDiv.innerHTML = '<h2 class ="mango">Mango $100/lb <br> Starfruit $100/lb <br> Passionfruit $100/lb <br> Jackfruit $100/lb <br> Durian $100/lb <br> Rambutan $100/lb <br> Mangosteen $100/lb</>'
 
+newDiv.appendChild(figDiv)
 }
 
 
