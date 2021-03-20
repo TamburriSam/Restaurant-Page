feaba951942aed9ef1ba3a07449b929729239535
tamburrisam <tamburrisam@gmail.com>
2021-03-20 11:57:23 -0400

    tab logic

--- src/index.js
+++ src/index.js
@@ -1,45 +1,41 @@
-import { home } from './renderElement.js';
-import {menu} from './menu.js';
-import {contact} from './contact.js';
+import { home } from "./renderElement.js";
+import { menu, removeMenu } from "./menu.js";
+import { contact } from "./contact.js";
 
-
-
-const contentDiv = document.querySelector('#content');
-contentDiv.setAttribute("id", "mainpic"); 
-
-//tabbed event listeners
-
-
+const contentDiv = document.querySelector("#content");
+contentDiv.setAttribute("id", "mainpic");
 
 home();
 
+const homeBtn = document.querySelector(".home");
+const contactBtn = document.querySelector(".contact");
+const menuBtn = document.querySelector(".menu");
 
+homeBtn.addEventListener("click", function () {
+  location.reload();
+});
 
-const homeBtn = document.querySelector('.home');
-const contactBtn = document.querySelector('.contact');
-const menuBtn = document.querySelector('.menu');
+menuBtn.addEventListener("click", function () {
+  homeBtn.style.textDecoration = "none";
+  menuBtn.style.textDecoration = "overline wavy";
+  if (contactBtn.style.textDecoration) {
+    contactBtn.style.textDecoration = "none";
+  }
+  menuBtn.disabled = 'true';
+ 
+  menu();
+});
 
+contactBtn.addEventListener("click", function () {
+  homeBtn.style.textDecoration = "none";
+  contactBtn.style.textDecoration = "overline wavy";
 
+  if (menuBtn.style.textDecoration) {
+    menuBtn.style.textDecoration = "none";
+document.getElementsByClassName('menu').disabled = 'false';
+        removeMenu();
+  }
+  contact();
+  contactBtn.disabled = 'true';
 
-homeBtn.addEventListener('click', function(){
-    location.reload();
-})
-
-menuBtn.addEventListener('click', function(){
-    homeBtn.style.textDecoration = 'none';
-    menuBtn.style.textDecoration = 'overline wavy';
-
-    menu();
-})
-
-contactBtn.addEventListener('click', function(){
-    homeBtn.style.textDecoration = 'none';
-    contactBtn.style.textDecoration = 'overline wavy';
-
-    contact();
-})
-
-
-
-//makeTitle(contentDiv)
-//makeSubtitle(contentDiv)
+});
